e92b46199ed265c2ee5f94518833c5e7d08f77f3
MinuraIddamalgoda <mini2476@gmail.com>
2019-01-04 10:06:54 +1100

    Setup initial CLion proj

---
+++ cmake-build-debug/CMakeFiles/Calendar_Exporter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Calendar_Exporter.dir/main.cpp.o"
+  "Calendar_Exporter.pdb"
+  "Calendar_Exporter"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Calendar_Exporter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <istream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
